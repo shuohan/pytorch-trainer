45669bb77cdc6e5b02614e6511f0f96538b31d2d
Shuo Han <shan50@jhu.edu>
2018-10-01 20:19:30 -0400

    Remove unet from the repo

--- pytorch_engine/blocks/__init__.py
+++ pytorch_engine/blocks/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from .plain_blocks import PostActivConvBlock, EncodingBlock, DecodingBlock
-from .plain_blocks import TransDownBlock, TransUpBlock
+from .plain_blocks import PostActivConvBlock
 from .residual_blocks import ResidualEncodingBlock
