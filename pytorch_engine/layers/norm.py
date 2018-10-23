2c120d09fdef771fc054c0b60a6f50bff98e264c
Shuo Han <shan50@jhu.edu>
2018-10-23 12:43:40 -0400

    Update Normalization defaults
    Do not set Normalization affine and track_running_stats

--- pytorch_engine/layers/norm.py
+++ pytorch_engine/layers/norm.py
@@ -8,24 +8,11 @@ from ..config import Configuration
 config = Configuration()
 
 if config.num_dims == 2:
-    InstanceNorm_ = InstanceNorm2d
-    BatchNorm_ = BatchNorm2d
+    InstanceNorm = InstanceNorm2d
+    BatchNorm = BatchNorm2d
 elif config.num_dims == 3:
-    InstanceNorm_ = InstanceNorm3d
-    BatchNorm_ = BatchNorm3d
-
-
-class InstanceNorm(InstanceNorm_):
-    """Customized instance normalization"""
-    def __init__(self, num_features):
-        super().__init__(num_features, affine=True, track_running_stats=True)
-
-
-class BatchNorm(BatchNorm_):
-    """Customized batch normalization"""
-    def __init__(self, num_features):
-        super().__init__(num_features, affine=True, track_running_stats=True)
-
+    InstanceNorm = InstanceNorm3d
+    BatchNorm = BatchNorm3d
 
 if config.norm == 'instance':
     Normalization = InstanceNorm
