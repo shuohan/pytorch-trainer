4ac7630f9cbf14c8374bad0b83bbf909bc2bf899
Shuo Han <shan50@jhu.edu>
2019-12-18 15:02:39 -0500

    Use additional inheritance for trainer validator
    Put the functions specific for the trainer and validator into another
    class which inherits from obserable. The abstract observable can then be
    used for evaluator.

--- pytorch_trainer/trainer_validator.py
+++ pytorch_trainer/trainer_validator.py
@@ -7,7 +7,109 @@ from .observer import Observable, Observer
 from .config import Config
 
 
-class Trainer(Observable):
+class _TraVal(Observable):
+    """Observable for :class:`Trainer` and :class:`Validator`.
+
+    Attributes:
+        data_loader (torch.utils.data.DataLoader): The data loader.
+        num_batches (int): The number of mini-batches.
+        num_epochs (int): The number of epochs.
+        models (dict[torch.nn.Module): The models to train or validate.
+        losses (dict[Buffer]): The calculated losses.
+        epoch (int): The current epoch.
+        batch (int): The current mini-batch.
+        dumps (dict[numpy.ndarray]): The intermediate results to dump into cpu.
+            It should store ``None`` if not :attr:`pytorch_trainer.Config.dump`.
+    
+    """
+    def __init__(self, data_loader, num_epochs=None):
+        self.data_loader = data_loader
+        self.num_epochs = num_epochs
+
+        self.num_batches = len(self.data_loader)
+        self.models = dict()
+        self.losses = dict()
+        self.epoch = 0
+        self.batch = 0
+        self.dumps = dict()
+
+        super().__init__()
+
+    def _move_models_to_cuda(self):
+        """Moves all models into cuda."""
+        for model in self.models.values():
+            model.cuda()
+
+    def _move_models_to_cuda(self):
+        """Moves all models into cpu."""
+        for model in self.models.values():
+            model.cpu()
+
+    def _transfer(self, data):
+        """Transfers data into GPU or CPU according to config.
+
+        Args:
+            data (tuple or torch.Tensor): The data to transfer.
+
+        Returns:
+            tuple or torch.Tensor: The transferred data.
+
+        """
+        return self._cuda(data) if Config.use_gpu else self._cpu(data)
+
+    def _cuda(self, data):
+        """Transfers data into GPU.
+
+        Args:
+            data (tuple or torch.Tensor): The data to transfer.
+
+        Returns:
+            tuple or torch.Tensor: The transferred data.
+
+        """
+        if type(data) is tuple:
+            return tuple(self._cuda(d) for d in data)
+        elif isinstance(data, torch.Tensor):
+            return data.cuda()
+
+    def _cpu(self, data):
+        """Dumps data into CPU.
+
+        Args:
+            data (tuple or torch.Tensor): The data to dump.
+
+        Returns:
+            tuple or torch.Tensor: The dumped data.
+
+        """
+        if type(data) is tuple:
+            return tuple(self._cpu(d) for d in data)
+        elif isinstance(data, torch.Tensor):
+            return data.cpu()
+
+    def _numpy(self, data):
+        """Converts :class:`torch.Tensor` to :class:`numpy.ndarray'.
+
+        Args:
+            data (torch.Tensor): The tensor to convert.
+
+        Returns:
+            numpy.ndarray: The converted.
+
+        """
+        return data.detach().cpu().numpy()
+
+    def _dump(self, name, data):
+        """Dumps data into :attr:`dumps` in CPU as :class:`numpy.ndarray`.
+
+        Args:
+            name (str): The name of the data, used as key in :attr:`dumps`.
+            data (torch.Tensor): The intermediate data to dump.
+        """
+        self.dumps[name] = self._numpy(data)
+
+
+class Trainer(_TraVal):
     """Abstract class for model training.
 
     """
@@ -45,10 +147,14 @@ class Trainer(Observable):
         raise NotImplementedError
 
 
-class Validator(Observable, Observer):
+class Validator(_TraVal, Observer):
     """Abstract class for model validation.
 
     """
+    def __init__(self, data_loader, num_epochs=None):
+        super().__init__(data_loader, num_epochs)
+        self.observable = None
+
     def update_on_training_start(self):
         """Initializes loss buffers."""
         self.num_epochs = self.observable.num_epochs
