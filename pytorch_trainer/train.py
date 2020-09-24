2fd3ad679f584a1448ce50cf356612ee6fd97a86
Shuo Han <shan50@jhu.edu>
2020-09-24 16:35:12 -0400

    Update train and remove unused utils

---
+++ pytorch_trainer/train.py
@@ -0,0 +1,239 @@
+import torch
+
+from ..observer import Subject, SubjectObserver
+from .utils import NamedData
+
+
+class _SimpleMixin:
+    """Mixin class for :class:`SimpleTrainer` and :class:`SimpleValidator`.
+
+    Attributes:
+        dataloader (torch.utils.data.DataLoader): Yields training data.
+    
+    """
+    def __init__(self, dataloader, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.dataloader = dataloader
+        self._input_cpu = None
+        self._input_cuda = None
+        self._output_cpu = None
+        self._output_cuda = None
+        self._truth_cpu = None
+        self._truth_cuda = None
+        self._input_names = None
+        self._truth_names = None
+        self._batch_ind = -1
+        self._value = None
+
+    @property
+    def num_batches(self):
+        return len(self.dataloder)
+
+    @property
+    def batch_size(self):
+        return self.dataloader.batch_size
+
+    @property
+    def batch_ind(self):
+        return self._batch_ind + 1
+
+    @property
+    def loss(self):
+        """Returns the loss value.
+
+        Returns:
+            number of iterable[number]: The loss values to keep track of.
+
+        """
+        return self._loss.item()
+
+    @property
+    def input_cpu(self):
+        """Returns the network input as :class:`torch.Tensor` on CPU."""
+        input_cpu = self._input_cpu 
+        if self._input_names is not None:
+            input_cpu = NamedData(self._input_names, input_cpu)
+        return input_cpu
+
+    @property
+    def input_cuda(self):
+        """Returns the network input as :class:`torch.Tensor` on CUDA."""
+        return self._input_cuda
+
+    @property
+    def output_cpu(self):
+        """Returns the network output as :class:`torch.Tensor` on CPU."""
+        if self._output_cpu is None:
+            self._output_cpu = self._output_cuda.detach().cpu().numpy()
+        return self._output_cpu
+
+    @property
+    def output_cuda(self):
+        """Returns the network output as :class:`torch.Tensor` on CUDA."""
+        return self._output_cuda
+
+    @property
+    def truth_cpu(self):
+        """Returns the network truth as :class:`torch.Tensor` on CPU."""
+        truth_cpu = self._truth_cpu 
+        if self._truth_names is None:
+            truth_cpu = NamedData(self._truth_names, truth_cpu)
+        return truth_cpu
+
+    @property
+    def truth_cuda(self):
+        """Returns the network truth as :class:`torch.Tensor` on CUDA."""
+        return self._truth_cuda
+
+    def _empty(self):
+        """Empties temporary tensors."""
+        self._input_cpu = None
+        self._input_cuda = None
+        self._output_cpu = None
+        self._output_cuda = None
+        self._truth_cpu = None
+        self._truth_cuda = None
+        self._input_names = None
+        self._truth_names = None
+
+    def _parse_input(self, data):
+        """Extracts input tensor."""
+        self._input_names, self._input_cpu, self._input_cuda = self._parse(data)
+
+    def _parse_truth(self. data):
+        """Extracts truth tensor."""
+        self._truth_names, self._truth_cpu, self._truth_cuda = self._parse(data)
+
+    def _parse(self, data):
+        """Extracts tensor names, on cpu, and on cuda."""
+        names, data_cpu = data if isinstance(data, NamedData) else (None, data)
+        data_cuda = data_cpu.cuda()
+        return names, data_cpu, data_cuda
+
+
+class SimpleTrainer(_SimpleMixin, Subject):
+    """Trains an algorithm with one network, optim, and loss function.
+
+    Attributes:
+        net (torch.nn.Module): The network to train.
+        optim (torch.optim.Optimizer): The network optimizer.
+        loss_func (torch.nn.Module): Calculates the loss.
+
+    Args:
+        num_epochs (int): The number of epochs.
+
+    """
+    def __init__(self, net, optim, dataloader, loss_func, num_epochs=100):
+        super().__init__(dataloader)
+        self.net = net
+        self.optim = optim
+        self.loss_func = loss_func
+        self._num_epochs = num_epochs
+        self._epoch_ind = -1
+
+    @property
+    def num_epochs(self):
+        return self._num_epochs
+
+    @property
+    def epoch_ind(self):
+        return self._epoch_ind + 1
+
+    def train(self):
+        """Trains the network."""
+        self.notify_observers_on_train_start()
+        for self._epoch_ind in range(self.num_epochs):
+            self.notify_observers_on_epoch_start()
+            for self._batch_ind, data in enumerate(self.dataloader):
+                self._parse_input(data[0])
+                self._parse_truth(data[1])
+                self.notify_observers_on_batch_start()
+                self._train_on_batch()
+                self.notify_observers_on_batch_end()
+            self.notify_observers_on_epoch_end()
+        self.notify_observers_on_train_end()
+
+    def _train_on_batch(self):
+        """Trains the network on each mini-batch.
+        
+        """
+        self.optim.zero_grad()
+        self._output_cuda = self.net(self._input_cuda)
+        self._loss = self.loss_func(self._output_cuda, self._truth_cuda)
+        self._loss.backward()
+        self.optim.step()
+
+
+class SimpleValidator(_SimpleMixin, SubjectObserver):
+    """Validates an algorithm with one network, optim, and loss_func.
+
+    Attributes:
+        step (int): Validate the network every this number of epochs.
+
+    """
+    def __init__(self, dataloader, step=10):
+        super().__init__(dataloader)
+        self.step = step
+
+    def _check_subject_type(self, subject):
+        assert isinstance(subject, SimpleTrainer)
+
+    @property
+    def num_epochs(self):
+        return self.subject.num_epochs
+
+    @property
+    def epoch_ind(self):
+        return self.subject.epoch_ind
+
+    def update_on_batch_start(self):
+        pass
+
+    def update_on_batch_end(self):
+        pass
+
+    def update_on_epoch_end(self):
+        if (self.epoch_ind % self.step) == 0:
+            self.notify_observers_on_epoch_start()
+            with torch.no_grad():
+                self.subject.net.eval() 
+                for self._batch_ind, data in enumerate(self.data_loader):
+                    self.notify_observers_on_batch_start()
+                    self._parse_input(data[0])
+                    self._parse_truth(data[1])
+                    self._validate_on_batch()
+                    self.notify_observers_on_batch_end()
+            self.notify_observers_on_epoch_end()
+
+    def _validate_on_batch(self):
+        """Validates the network on a batch."""
+        self._output_cuda = self.subject.net(self._input_cuda)
+        self._loss = self.subject.loss_func(self._output_cuda, self._truth_cuda)
+
+
+class SimpleEvaluator(SubjectObserver):
+    """Evaluates an algorithm with one network, optim, and loss function.
+
+    Attributes:
+        eval_funcs (dict): The :class:`str` name and function pairs of the
+            evaluation functions. The functions should be defined as
+            ``func(output, truth)``.
+    
+    """
+    def __init__(self, eval_funcs):
+        super().__init__()
+        self.eval_funcs = eval_funcs
+
+    def _check_subject_type(self, subject):
+        assert isinstance(subject, _SimpleMixin)
+
+    @property
+    def batch_size(self):
+        return self.subject.batch_size
+
+    def update_on_batch_end(self):
+        output_cuda = self.subject.output_cuda
+        truth_cuda = self.subject.truth_cuda
+        with torch.no_grad():
+            for name, func in self.eval_funcs.items():
+                setattr(self, name, func(output_cuda, truth_cuda))
--- pytorch_trainer/utils.py
+++ pytorch_trainer/utils.py
@@ -1,155 +1,21 @@
-import torch
-
-from .config import Config, Reduction
+from collections import namedtuple
 
 
-def convert_th_to_np(data):
-    """Converts :class:`torch.Tensor` to :class:`numpy.ndarray'.
+NamedData = namedtuple('NamedData', ['name', 'data'])
+"""Data with its name.
 
-    Args:
-        data (torch.Tensor): The tensor to convert.
-
-    Returns:
-        numpy.ndarray: The converted.
-
-    """
-    return data.detach().cpu().numpy()
-
-
-def _eval(instance, func, Type, *args, **kwargs):
-    """Calls the function of the instance.
-
-    Args:
-        data (tuple or list or dict or Type): The instance to call the function
-            from.
-
-    Returns:
-        tuple or list or dict or Type: The results of the function called.
-
-    """
-    if type(instance) is tuple:
-        return tuple(_eval(i, func, Type, *args, **kwargs) for i in instance)
-    elif type(instance) is list:
-        return list(_eval(i, func, Type, *args, **kwargs) for i in instance)
-    elif type(instance) is dict:
-        return {k: _eval(v, func, Type, *args, **kwargs)
-                for k, v in instance.items()}
-    elif isinstance(instance, Type):
-        return getattr(instance, func)(*args, **kwargs)
-
-
-def transfer_data_to_cuda(data, *args, **kwargs):
-    """Transfers data into GPU.
-
-    Args:
-        data (tuple or list or dict or torch.Tensor): The data to transfer.
-
-    Returns:
-        tuple or list or dict or torch.Tensor: The transferred data.
-
-    """
-    return _eval(data, 'cuda', torch.Tensor, *args, **kwargs)
-
-
-def transfer_data_to_cpu(data, *args, **kwargs):
-    """Transfers data into CPU.
-
-    Args:
-        data (tuple or list or dict or torch.Tensor): The data to transfer.
-
-    Returns:
-        tuple or list or dict or torch.Tensor: The transferred data.
-
-    """
-    return _eval(data, 'cpu', torch.Tensor, *args, **kwargs)
-
-
-def transfer_models_to_cuda(models, *args, **kwargs):
-    """Transfers models into GPU.
+Attributes:
+    name (str): The name of the data.
+    data (numpy.ndarray): The data.
     
-    Args:
-        models (tuple or list or dict or torch.nn.Module): The models to
-            transfer.
-
-    Returns:
-        tuple or list or dict or torch.nn.Module: The transferred models.
-    
-    """
-    return _eval(models, 'cuda', torch.nn.Module, *args, **kwargs)
+"""
 
 
-def transfer_models_to_cpu(models, *args, **kwargs):
-    """Transfers models into CPU.
-    
-    Args:
-        models (tuple or list or dict or torch.nn.Module): The models to
-            transfer.
-
-    Returns:
-        tuple or list or dict or torch.nn.Module: The transferred models.
-    
-    """
-    return _eval(models, 'cpu', torch.nn.Module, *args, **kwargs)
-
-
-def set_models_to_train(models, *args, **kwargs):
-    """Sets models into train mode.
-    
-    Args:
-        models (tuple or list or dict or torch.nn.Module): The models to change.
-
-    Returns:
-        tuple or list or dict or torch.nn.Module: The transferred models.
-    
-    """
-    return _eval(models, 'train', torch.nn.Module, *args, **kwargs)
-
-
-def set_models_to_eval(models, *args, **kwargs):
-    """Sets models into eval mode.
-    
-    Args:
-        models (tuple or list or dict or torch.nn.Module): The models to change.
-
-    Returns:
-        tuple or list or dict or torch.nn.Module: The transferred models.
-    
-    """
-    return _eval(models, 'eval', torch.nn.Module, *args, **kwargs)
-
-
-def reduce(value, dims=tuple()):
-    """Reduces value if it is not a scaler.
+def count_trainable_paras(net):
+    """Counts the trainable parameters of a network.
 
     Args:
-        value (torch.Tensor): The tensor to reduce.
-        dims (int or tuple[int]): The reduction axes.
-   
-    Raises:
-        RuntimeError: :attr:`Config.reduction` is not in :class:`Recuction`.
-    
-    """
-    if len(value.shape) > 0:
-        if Config.reduction is Reduction.MEAN:
-            if len(dims) > 0:
-                value = torch.mean(value, dims)
-            else:
-                value = torch.mean(value)
-        elif Config.reduction is Reduction.SUM:
-            if len(dims) > 0:
-                value = torch.sum(value, dims)
-            else:
-                value = torch.sum(value)
-        else:
-            raise RuntimeError(Config.reduction, 'is not supported.')
-    return value
-
-
-def count_trainable_paras(model):
-    """Counts the trainable parameters of a model.
-
-    Args:
-        model (torch.nn.Module): The model to count.
+        net (torch.nn.Module): The network to count.
 
     """
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return sum(p.numel() for p in net.parameters() if p.requires_grad)
