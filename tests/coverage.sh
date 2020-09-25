890467f740d23d1647dfb278c8b274b00beeeb93
Shuo Han <shan50@jhu.edu>
2020-09-25 13:00:25 -0400

    Add local code coverage badge

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -4,9 +4,9 @@ dir=$(realpath $(dirname $0)/..)
 docker run --gpus device=1 --rm -v $dir:$dir \
     --user $(id -u):$(id -g) -w $dir/tests \
     -e PYTHONPATH=$dir -t \
-    pytorch-shan coverage run -m pytest
+    pytorch-shan coverage run --source=../pytorch_trainer -m pytest .
 
 docker run --gpus device=1 --rm -v $dir:$dir \
     --user $(id -u):$(id -g) -w $dir/tests \
     -e PYTHONPATH=$dir -t \
-    pytorch-shan coverage report -m ../pytorch_trainer/*.py
+    pytorch-shan coverage report -m 
